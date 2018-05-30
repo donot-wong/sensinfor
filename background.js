c1859f440853187852b90bb0aa5f103fe8cc5e66
donot <wang@sqvds.com>
2018-05-30 11:45:51 +0800

    add click to clipbord

--- background.js
+++ background.js
@@ -54,17 +54,49 @@ function updateIcon(name) {
   chrome.browserAction.setIcon({path: name + ".png"});
 }
 
+function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea")
+
+    textArea.style.position = 'fixed'
+    textArea.style.top = 0
+    textArea.style.left = 0
+    textArea.style.width = '2em'
+    textArea.style.height = '2em'
+    textArea.style.padding = 0
+    textArea.style.border = 'none'
+    textArea.style.outline = 'none'
+    textArea.style.boxShadow = 'none'
+    textArea.style.background = 'transparent'
+    textArea.value = text
+
+    document.body.appendChild(textArea)
+
+    textArea.select()
+
+    try {
+      var msg = document.execCommand('copy') ? 'sucess' : 'failed'
+      // console.log('复制内容 ' + msg)
+    } catch (err) {
+      console.log('不能使用这种方法复制内容')
+    }
+
+    document.body.removeChild(textArea)
+}
+
+
 function show(title, content, tagname) {
   var notice = new Notification(title, {
     body: content,
     icon: "icon48.png",
     tag: tagname, // 可以加一个tag
   });
-  // function clicked(){
-  //   windows.open(content, '_blank');
-  //   notice.close();
-  // }
-  // notice.addEventListener('click',clicked);
+  
+  // notice['onclose'] = function(event){
+  //     window.open(content, '_blank');
+  // };
+  notice['onclick'] = function(event){
+      copyTextToClipboard(content);
+  };
 }
 
 
