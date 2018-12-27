043dedeab5644742fee24581c47aafdad4d7a005
donot <donot@donot.me>
2018-12-27 22:31:02 +0800

    新增记录 清空功能

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,18 @@
-var data = "";
+function dedupe(array){
+	return Array.from(new Set(array));
+}
+
+var data = new Array();
 for(var i = 0; i < localStorage.length; i++)
 {
 	var key = localStorage.key(i);
-	data = data + localStorage.getItem(key) + '\n';
+	data.push(localStorage.getItem(key));
 }
+
+data = dedupe(data);
+
 if (data) {
-	document.getElementById("history").value = data;
+	document.getElementById("history").value = data.join('\n');
 }
 
 
